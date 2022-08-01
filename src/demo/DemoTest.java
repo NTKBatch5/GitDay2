2e8da2702b6258d8cdd42e9ae8fc91118f005145
ntkb4 <ntkjasur@gmail.com>
2022-08-01 19:06:25 -0400

    Implemented US123 Automation Script

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,5 +5,12 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
+	
+	
+	
+	//for US123... 
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
